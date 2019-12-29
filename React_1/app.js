ac0b042addae28d54027b6d3e2d3e4aeab320ab0
gapinska <gapinska.mon@gmail.com>
2019-12-29 22:16:51 +0100

    React task components

--- React_1/app.js
+++ React_1/app.js
@@ -1,26 +1,32 @@
 //komponent funkcyjny
 
-const App = () => {
-    return (
-        <>
-        <h1>First component</h1>        
-        </>
-    )
+const Header = () => {
+    return <h1>Witaj na stronie</h1>
 }
 
 //komponent klasowy
 
-class App2 extends React.Component {
-    state = {
-        number: 0
-    }
+class Blog extends React.Component {
+    // state = {
+    //     number: 0
+    // }
     render(){
         return (
             <section>
-                <h2>Komponent klasowy this.state.number</h2>
+                <h2>Artykuł</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae esse voluptas tempore reprehenderit, dicta, nam at officiis perspiciatis incidunt quis, corporis magni ullam delectus sint dolore nostrum omnis illo ab!</p>
             </section>
         )
     }
+}
+
+const App = () => {
+    return (
+        <>
+        <Header/>
+        <Blog/>        
+        </>
+    )
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'))
